f7e3ad5f795527487b2cea1ebc29a6d16f213910
John Chabot <johnchabot2013@gmail.com>
2019-03-30 07:45:00 -0500

    first contact and incremental compilation

--- clients/vscode/src/commands/runProvider.ts
+++ clients/vscode/src/commands/runProvider.ts
@@ -1,29 +1,26 @@
 import { IKosCommand } from './types';
-// import { selectOrCreateTerminal } from './terminalUtilities';
-// import { commandOnPath, platformTelnetClient, platformTelnetArguments } from './commandUtilities';
+// tslint:disable-next-line:import-name
 import Telnet from 'telnet-client';
 
 const client = new Telnet();
 
 export const runProvider: IKosCommand = {
   command: 'kos.runCurrentFile',
-  commandCallback: () => {
-    // const terminal = selectOrCreateTerminal('KOS Telnet');
-    // terminal.show();
+  commandCallback: async () => {
+    try {
+      const result = await client.connect({
+        timeout: 3000,
+        host: '127.0.0.1',
+        port: 5410,
+        shellPrompt: '>',
+      });
 
-    // commandOnPath(platformTelnetClient())
-    //   .then(
-    //     (success) => {
-    //       if (success) {
-    //         terminal.sendText(`${platformTelnetClient()} ${platformTelnetArguments()}`);
-    //       } else {
-    //         terminal.sendText(`echo "${platformTelnetClient()} not found."`);
-    //       }
-    //       return success;
-    //     },
-    //     (error) => {
-    //       console.log('failure');
-    //       console.log(error);
-    //     });
+      console.log(result);
+
+      await client.exec('1');
+      await client.exec('fart');
+    } catch (e) {
+      console.log(e);
+    }
   },
 };
