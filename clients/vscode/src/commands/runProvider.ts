154a50b00840f80c07ec5f0c8b0ec4c7e3c56259
John Chabot <johnchabot@2013@gmail.com>
2019-03-30 14:22:32 -0700

    telnet logging may need to rethink approach

--- clients/vscode/src/commands/runProvider.ts
+++ clients/vscode/src/commands/runProvider.ts
@@ -4,21 +4,59 @@ import Telnet from 'telnet-client';
 
 const client = new Telnet();
 
+client.on('connect', () => {
+  console.log('connect');
+});
+
+client.on('ready', (data) => {
+  console.log('ready', data.toString());
+});
+
+client.on('writedone', () => {
+  console.log('writedone');
+});
+
+client.on('writedone', () => {
+  console.log('writedone');
+});
+
+client.on('data', (data) => {
+  console.log('data', data.toString());
+});
+
+client.on('timeout', () => {
+  console.log('timeout');
+});
+
+client.on('error', () => {
+  console.log('error');
+});
+
+client.on('end', () => {
+  console.log('end');
+});
+
+client.on('close', () => {
+  console.log('close');
+});
+
 export const runProvider: IKosCommand = {
   command: 'kos.runCurrentFile',
   commandCallback: async () => {
     try {
-      const result = await client.connect({
-        timeout: 3000,
-        host: '127.0.0.1',
-        port: 5410,
-        shellPrompt: '>',
-      });
+      if (client.state == null) {
+        await client.connect({
+          timeout: 30000,
+          host: '127.0.0.1',
+          port: 5410,
+          shellPrompt: '>',
+          echoLines: 0,
+        });
+      }
 
-      console.log(result);
+      const thing = await client.exec('1\r\n', { timeout: 30000, shellPrompt: '����' });
 
-      await client.exec('1');
-      await client.exec('fart');
+      console.log(thing);
     } catch (e) {
       console.log(e);
     }
