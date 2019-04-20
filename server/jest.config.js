dfa11823116efed0ffbe5ffed398990516f63976
John Chabot <johnchabot2013@gmail.com>
2019-04-20 18:18:15 -0500

    added code coverage

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,8 +2,22 @@ const path = require('path');
 
 const config = {
   preset: 'ts-jest',
+  reporters: [
+    "default",
+    "jest-junit"
+  ],
+  coverageReporters: [
+    "cobertura",
+    "html"
+  ],
   globals: {
     'ts-jest': {
+      suiteName: "jest tests",
+      classNameTemplate: "{classname} - {title}",
+      titleTemplate: "{classname} - {title}",
+      ancestorSeparator: " > ",
+      usePathForSuiteName: true,
+      output: path.join(__dirname, "TEST.xml"),
       tsConfig: path.join(__dirname, 'tsconfig.json')
     }
   },
