47094cc5804474b085714387a1bd2d5996d7444f
John Chabot <johnchabot2013@gmail.com>
2019-05-11 06:13:57 -0500

    More commenting of utils

--- server/src/utilities/diagnosticsUtils.ts
+++ server/src/utilities/diagnosticsUtils.ts
@@ -5,6 +5,14 @@ import {
 } from 'vscode-languageserver';
 import { languageServer } from './constants';
 
+/**
+ * This provides thin wrapper around Diagnostic.create where the server name is already included
+ * @param range range the diagnostics applies
+ * @param message message for the diagnostic
+ * @param severity severity of the diagnostic
+ * @param code error code
+ * @param relatedInformation other related information 
+ */
 export const createDiagnostic = (
   range: Range,
   message: string,
