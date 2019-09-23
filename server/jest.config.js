e9be191ef82152bf12e59d0783fc3804ff2a67a9
John Chabot <johnchabot2013@gmail.com>
2019-09-22 22:36:46 -0400

    Update test config

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,28 +1,23 @@
+'use strict';
 const path = require('path');
 
 const config = {
   preset: 'ts-jest',
-  reporters: [
-    "default",
-    "jest-junit"
-  ],
-  coverageReporters: [
-    "html",
-    "cobertura",
-  ],
-  rootDir: "src",
+  reporters: ['default', 'jest-junit'],
+  coverageReporters: ['html', 'cobertura'],
+  rootDir: 'src',
   globals: {
     'ts-jest': {
-      suiteName: "jest tests",
-      classNameTemplate: "{classname} - {title}",
-      titleTemplate: "{classname} - {title}",
-      ancestorSeparator: " > ",
+      suiteName: 'jest tests',
+      classNameTemplate: '{classname} - {title}',
+      titleTemplate: '{classname} - {title}',
+      ancestorSeparator: ' > ',
       usePathForSuiteName: true,
-      output: path.join(__dirname, "TEST.xml"),
-      tsConfig: path.join(__dirname, 'tsconfig.json')
-    }
+      output: path.join(__dirname, 'TEST.xml'),
+      tsConfig: path.join(__dirname, 'tsconfig.json'),
+    },
   },
-  collectCoverage: true
+  collectCoverage: true,
 };
 
 module.exports = config;
